157423eb7216f6770755baf2d4b8bf3fe4a44bd7
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-06-17 03:22:47 +0700

    added validasi

--- app/src/main/java/com/example/sepada/ui/main/user/tamu/DetailPengajuanFragment.java
+++ app/src/main/java/com/example/sepada/ui/main/user/tamu/DetailPengajuanFragment.java
@@ -1,5 +1,6 @@
 package com.example.sepada.ui.main.user.tamu;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,6 +12,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.sepada.R;
 import com.example.sepada.databinding.FragmentDetailPengajuanBinding;
@@ -41,6 +45,34 @@ public class DetailPengajuanFragment extends Fragment {
             binding.btnDownload.setVisibility(View.VISIBLE);
         }else {
             binding.btnDownload.setVisibility(View.GONE);
+        }
+
+        if (!getArguments().getString("status").equals("1")) {
+            Dialog dialog = new Dialog(getContext());
+            dialog.setContentView(R.layout.layout_alasan_verifikasi);
+            dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+            EditText etAlasan = dialog.findViewById(R.id.etAlasanVerifikasi);
+            TextView tvStatus = dialog.findViewById(R.id.tvStatus);
+
+            Button btnOKe = dialog.findViewById(R.id.btnOke);
+
+            if (getArguments().getString("status").equals("2")) {
+                tvStatus.setText("Disetujui");
+                tvStatus.setTextColor(getContext().getColor(R.color.main));
+            }else {
+                tvStatus.setText("Ditolak");
+                tvStatus.setTextColor(getContext().getColor(R.color.red));
+
+            }
+
+            etAlasan.setText(getArguments().getString("alasan_verifikasi"));
+            dialog.show();
+            btnOKe.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
         }
 
 
